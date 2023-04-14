a3ee8d867afc9f34e57e6cc3242aa32e173f86c1
uavony <205705911@qq.com>
2023-04-14 22:33:14 +0800

    update:system

--- go.mod
+++ go.mod
@@ -16,11 +16,16 @@ require (
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.9 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.8.0 // indirect
 	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
 	golang.org/x/crypto v0.5.0 // indirect
 	golang.org/x/net v0.7.0 // indirect
@@ -39,6 +44,7 @@ require (
 	github.com/golang-jwt/jwt/v5 v5.0.0-rc.2
 	github.com/google/uuid v1.3.0
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/labstack/gommon v0.4.0
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mattn/go-sqlite3 v1.14.16
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
@@ -51,6 +57,7 @@ require (
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/spf13/viper v1.15.0 // indirect
 	github.com/subosito/gotenv v1.4.2 // indirect
+	go.uber.org/zap v1.24.0
 	golang.org/x/exp v0.0.0-20230321023759-10a507213a29
 	golang.org/x/sys v0.5.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
--- service/system.go
+++ service/system.go
@@ -2,17 +2,116 @@ package service
 
 import (
 	"context"
+	"encoding/json"
 	"net/http"
+	"os"
 	"uamemos/api"
 	"uamemos/common"
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
+	"github.com/labstack/gommon/log"
+	"go.uber.org/zap"
 )
 
 func (s *Service) registerSystemRoutes(g *gin.RouterGroup) {
 	g.GET("/ping", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, composeResponse(s.Profile))
+	})
+	g.GET("/status", func(ctx *gin.Context) {
+		hostUserType := api.Host
+		hostUserFind := api.UserFind{
+			Role: &hostUserType,
+		}
+		hostUser, err := s.Store.FindUser(ctx, &hostUserFind)
+		if err != nil && common.ErrorCode(err) != common.NotFound {
+			ctx.String(http.StatusInternalServerError, "Failed to find host user")
+			return
+		}
+		if hostUser != nil {
+			hostUser.OpenID = ""
+			hostUser.Email = ""
+		}
+		systemStatus := api.SystemStatus{
+			Host:               hostUser,
+			Profile:            *s.Profile,
+			DBSize:             0,
+			AllowSignUp:        false,
+			IgnoreUpgrade:      false,
+			DisablePublicMemos: false,
+			AdditionalStyle:    "",
+			AdditionalScript:   "",
+			CustomizedProfile: api.CustomizedProfile{
+				Name:        "uamemos",
+				LogoURL:     "",
+				Description: "",
+				Locale:      "zh",
+				Appearance:  "system",
+				ExternalURL: "",
+			},
+			StorageServiceID: api.DatabaseStorage,
+			LocalStoragePath: "",
+		}
+		systemSettingList, err := s.Store.FindSystemSettingList(ctx, &api.SystemSettingFind{})
+		if err != nil {
+			ctx.String(http.StatusInternalServerError, "Failed to find system setting list")
+			return
+		}
+		for _, systemSetting := range systemSettingList {
+			if systemSetting.Name == api.SystemSettingServiceIDName || systemSetting.Name == api.SystemSettingSecretSessionName || systemSetting.Name == api.SystemSettingOpenAIConfigName {
+				continue
+			}
+			var baseValue any
+			err := json.Unmarshal([]byte(systemSetting.Value), &baseValue)
+			if err != nil {
+				log.Warn("Failed to unmarshal system setting value", zap.String("setting name", systemSetting.Name.String()))
+				continue
+			}
+
+			if systemSetting.Name == api.SystemSettingAllowSignUpName {
+				systemStatus.AllowSignUp = baseValue.(bool)
+			} else if systemSetting.Name == api.SystemSettingIgnoreUpgradeName {
+				systemStatus.IgnoreUpgrade = baseValue.(bool)
+			} else if systemSetting.Name == api.SystemSettingDisablePublicMemosName {
+				systemStatus.DisablePublicMemos = baseValue.(bool)
+			} else if systemSetting.Name == api.SystemSettingAdditionalStyleName {
+				systemStatus.AdditionalStyle = baseValue.(string)
+			} else if systemSetting.Name == api.SystemSettingAdditionalScriptName {
+				systemStatus.AdditionalScript = baseValue.(string)
+			} else if systemSetting.Name == api.SystemSettingCustomizedProfileName {
+				customizedProfile := api.CustomizedProfile{}
+				err := json.Unmarshal([]byte(systemSetting.Value), &customizedProfile)
+				if err != nil {
+					ctx.String(http.StatusInternalServerError, "Failed to unmarshal system setting customized profile value")
+					return
+				}
+				systemStatus.CustomizedProfile = customizedProfile
+			} else if systemSetting.Name == api.SystemSettingStorageServiceIDName {
+				systemStatus.StorageServiceID = int(baseValue.(float64))
+			} else if systemSetting.Name == api.SystemSettingLocalStoragePathName {
+				systemStatus.LocalStoragePath = baseValue.(string)
+			}
+		}
+		userID, ok := ctx.Get(getUserIDContextKey())
+		if ok {
+			userID, _ := userID.(int)
+			user, err := s.Store.FindUser(ctx, &api.UserFind{
+				ID: &userID,
+			})
+			if err != nil {
+				ctx.String(http.StatusInternalServerError, "Failed to find user")
+				return
+			}
+			if user != nil && user.Role == api.Host {
+				fi, err := os.Stat(s.Profile.DSN)
+				if err != nil {
+					ctx.String(http.StatusInternalServerError, "Failed to read database fileinfo")
+					return
+				}
+				systemStatus.DBSize = fi.Size()
+			}
+		}
+		ctx.JSON(http.StatusOK, composeResponse(systemStatus))
 	})
 }
 
